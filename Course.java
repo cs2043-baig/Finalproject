99799623890f526cef60894095e0478009148b6b
jay-m <jrugombo@unb.ca>
2020-12-01 21:38:21 -0400

    Course class

---
+++ Course.java
@@ -0,0 +1,39 @@
+public class Course {
+    private String name;
+    private String courseID;
+    private String professor;
+    private String preReq;
+    private int numStudents;
+
+    public Course(String name, String courseID, String professor, String preReq, int numStudents){
+        this.name = name;
+        this.courseID = courseID;
+        this.professor = professor;
+        this.preReq = preReq;
+        this.numStudents = numStudents;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    String getCourseID(){
+        return courseID;
+    }
+
+    String getProfessor(){
+        return professor;
+    }
+
+    String getPreReq(){
+        return preReq;
+    }
+
+    int getNumberOfStudents(){
+        return numStudents;
+    }
+
+    public String toString(){
+      return name + ", " + courseID + "\nThe professor is: "+ professor +"\nPrerequisite: "+preReq;
+   }
+}
