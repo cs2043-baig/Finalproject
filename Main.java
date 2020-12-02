8475bd8d42d174e7874ac1bb200e8dfa5e765d81
jay-m <jrugombo@unb.ca>
2020-12-01 21:50:12 -0400

    S and P can log in

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String[] password;
+        String[] username;
         System.out.println("Are you a Student or Professor? Type: \"P\" for professor and \"S\" for student.");
         char ch = scan.next().charAt(0);
         // System.out.println(ch);
@@ -10,20 +12,17 @@ public class Main{
             ch = scan.next().charAt(0);
         }
         if(ch == 'P'){
-            System.out.println("You picked Professor.");
-        }else{
-            // System.out.println("You picked Student.");
-            String[] Password = new String[]{"123a","456b","789c","101d","121e"};
+            password = new String[]{"123a","456b","789c","101d","121e"};
             boolean iv=false;
-            String[] Username = new String[]{"Negar","Ahmed","Jean","Max","Shehryar"};
+            username = new String[]{"Negar","Ahmed","Jean","Max","Shehryar"};
             System.out.print("Enter Username : ");
-            String username = scan.next();
-            for (int i =0; i<Username.length ; i++){
-                if (username.equals(Username[i])){
+            String name = scan.next();
+            for (int i =0; i<username.length ; i++){
+                if (name.equals(username[i])){
                     System.out.print("Enter Password : ");
-                    String password = scan.next();
+                    String psswd = scan.next();
                     iv = true;
-                    if(password.equals(Password[i])){
+                    if(psswd.equals(password[i])){
                         System.out.println("Access Granted! Welcome!");
                         //Your account details//
                         break;
@@ -35,7 +34,32 @@ public class Main{
             if(iv==false){
                 System.out.println("Invalid Username!");
                 System.out.println("If you want to create an account contact University Administration at XYZ@abc.ca");
-            }//11212
+            }
+        }else{
+            // System.out.println("You picked Student.");
+            password = new String[]{"123a","456b","789c","101d","121e"};
+            boolean iv=false;
+            username = new String[]{"Negar","Ahmed","Jean","Max","Shehryar"};
+            System.out.print("Enter Username : ");
+            String name = scan.next();
+            for (int i =0; i<username.length ; i++){
+                if (name.equals(username[i])){
+                    System.out.print("Enter Password : ");
+                    String passwd = scan.next();
+                    iv = true;
+                    if(passwd.equals(password[i])){
+                        System.out.println("Access Granted! Welcome!");
+                        //Your account details//
+                        break;
+                    }else{
+                        System.out.println("Invalid Password!");
+                    }
+                }
+            }
+            if(iv==false){
+                System.out.println("Invalid Username!");
+                System.out.println("If you want to create an account contact University Administration at XYZ@abc.ca");
+            }
         }
             
     }
