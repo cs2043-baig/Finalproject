89c99c194ca5b34306ac52d0a39b54537feb2b19
cs2043-baig <73848600+cs2043-baig@users.noreply.github.com>
2020-12-02 15:54:10 -0330

    Create Professor.java

---
+++ UniversityRegistrationManagement/src/Professor.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Professor {
+    private String name;
+    private String email;
+    private int id;
+    private String pass
+    private Course[] courses;
+Scaner scan = new Scanner(System.in);
+    // construct a new student with given fields
+public Student(String name, String pass, String email, int id, Course[] courses){
+    this.name = name;
+    this.email = email;
+    this.id = id;
+    this.pass=pass;
+    this.course = course;
+    }
