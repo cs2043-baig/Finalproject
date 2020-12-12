2f258ee3726c8d5696ff27048d28ae9978a9b5e0
Jean-Marc <55032257+jean-marcc@users.noreply.github.com>
2020-12-12 14:27:44 -0400

    JavaFX

---
+++ UniversityRegistrationManagement/src/User.java
@@ -0,0 +1,64 @@
+
+public class User {
+    private String username;
+    private String password;
+    /*private String name;
+    private String email;*/
+
+    public User(String username, String password/*, String name, String email*/){
+    	super();
+        this.username = username;
+        this.password = password;
+        /*this.name = name;
+        this.email = email;*/
+        
+    }
+    
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	/*public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}*/
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (obj instanceof User) {
+			if (this.getUsername().equals(((User) obj).getUsername())
+					&& this.getPassword().equals(((User) obj).getPassword())) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		return false;
+	}
+}
